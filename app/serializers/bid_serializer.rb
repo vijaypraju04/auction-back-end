8182d1cfcb47172b868298bea16adc5b241e7bbe
github username <github email address>
2017-12-06 13:32:13 -0500

    added more to serializers

--- app/serializers/bid_serializer.rb
+++ app/serializers/bid_serializer.rb
@@ -1,5 +1,5 @@
 class BidSerializer < ActiveModel::Serializer
-  attributes :id, :amount
+  attributes :id, :amount, :user_id, :auction_id
   belongs_to :user
   belongs_to :auction
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,3 @@ auction1 = Auction.create(item: "http://www.toptenz.net/wp-content/uploads/2010/
 auction2 = Auction.create(item: "https://media1.popsugar-assets.com/files/thumbor/Ut96-35GwDhUIfbZFzgH-jQi4ak/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/04/12/079/n/1922243/26282b73_edit_img_cover_file_14690959_1425494100_mastiff/i/10-Most-Expensive-Dog-Breeds.jpg", title: "A Tibetan Mastiff", description: "A very cute dog", value: 10000, end_date: 'October 12 2018', creator_id: 2)
 
 auction3 = Auction.create(item: "http://webneel.com/daily/sites/default/files/images/daily/10-2013/3-most-expensive-painting-portrait-adele-bloch-bauer.jpg", title: "Portrait of Adele Bloch-Bauer I", description: "This painting was painted by Gustav Klimt in 1907", value: 30000, end_date: 'December 25 2017', creator_id: 1)
-
-bid1 = Bid.create(user_id: 1, auction_id: 1, amount: 0)
-bid2 = Bid.create(user_id: 2, auction_id: 2, amount: 0)
-bid3 = Bid.create(user_id: 3, auction_id: 3, amount: 0)
-bid4 = Bid.create(user_id: 4, auction_id: 3, amount: 0)
