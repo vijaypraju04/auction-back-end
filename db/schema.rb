3ed2ae1d793f7fbef0dd194aa896c115007a3051
github username <github email address>
2017-12-06 11:09:58 -0500

    added dates to auction table and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205204133) do
+ActiveRecord::Schema.define(version: 20171206160618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171205204133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.string "end_date"
   end
 
   create_table "bids", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ user2 = User.create(username: "frawlcity", password: "kenneth", amount: 20000)
 user3 = User.create(username: "kendog", password: "kenneth", amount: 20000)
 user4 = User.create(username: "itsjustdiego", password: "kenneth", amount: 20000)
 
-auction1 = Auction.create(item: "http://www.toptenz.net/wp-content/uploads/2010/06/Sultans-golden-cake-most-expensive-desserts.jpg", title: "Sultan's Golden Cake", description: "A great pastry", value: 5000)
+auction1 = Auction.create(item: "http://www.toptenz.net/wp-content/uploads/2010/06/Sultans-golden-cake-most-expensive-desserts.jpg", title: "Sultan's Golden Cake", description: "A great pastry", value: 5000, end_date: 'October 10 2018')
 
-auction2 = Auction.create(item: "https://media1.popsugar-assets.com/files/thumbor/Ut96-35GwDhUIfbZFzgH-jQi4ak/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/04/12/079/n/1922243/26282b73_edit_img_cover_file_14690959_1425494100_mastiff/i/10-Most-Expensive-Dog-Breeds.jpg", title: "A Tibetan Mastiff", description: "A very cute dog", value: 10000)
+auction2 = Auction.create(item: "https://media1.popsugar-assets.com/files/thumbor/Ut96-35GwDhUIfbZFzgH-jQi4ak/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/04/12/079/n/1922243/26282b73_edit_img_cover_file_14690959_1425494100_mastiff/i/10-Most-Expensive-Dog-Breeds.jpg", title: "A Tibetan Mastiff", description: "A very cute dog", value: 10000, end_date: 'October 12 2018')
 
-auction3 = Auction.create(item: "http://webneel.com/daily/sites/default/files/images/daily/10-2013/3-most-expensive-painting-portrait-adele-bloch-bauer.jpg", title: "Portrait of Adele Bloch-Bauer I", description: "This painting was painted by Gustav Klimt in 1907", value: 30000)
+auction3 = Auction.create(item: "http://webneel.com/daily/sites/default/files/images/daily/10-2013/3-most-expensive-painting-portrait-adele-bloch-bauer.jpg", title: "Portrait of Adele Bloch-Bauer I", description: "This painting was painted by Gustav Klimt in 1907", value: 30000, end_date: 'December 25 2017')
 
 bid1 = Bid.create(user_id: 1, auction_id: 1, amount: 0)
 bid2 = Bid.create(user_id: 2, auction_id: 2, amount: 0)
